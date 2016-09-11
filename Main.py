6190559fbb4a02adeb8c7fdf1fb3c2fc3353de56
MarsRedSky <jmsiegel@ucsc.edu>
2016-09-11 08:31:19 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,62 +1,37 @@
-import re 
+import re
 from collections import Counter
 import csv
+import argparse
+import argparse 
 
-from sys import argv
+parser = argparse.ArgumentParser()
+parser.add_argument('filename') 
+parser.add_argument('-j', action='store_true')
+args = parser.parse_args()
+
+file = open(args.filename, 'r') 
+file = file.read().lower()
+
+#deletes comments from dyads
+file = re.sub("[\[].*?[\]]"," ", file)
+file = re.sub("[\(].*?[\)]"," ", file)
+
+#Jeffersonian, deletes ? ., or not * : ? . 
+if args.j:	
+	text = file.replace("?","").replace(".","")
 
 
-script, filename = argv
-
-
-print "Would you like a Jeffersonian analysis? \n Type 1 for yes. \n Type 2  for no."
-
-jeff = 0
-
-def choice(x):
-	global jeff
-	try:
-		jeff = int(raw_input())
-		if jeff == 1 or jeff == 2:
-			return jeff
-		else:	
-			print "please select 1 or 2."
-			choice(jeff)
-	except ValueError:
-		print("please select 1 or 2.")
-		return choice(jeff)
-   	
-
-choice(jeff)
+else:
+	file = re.sub("[\*].*?[\*]", " ", file)
+	text = file.replace("*","").replace(":","").replace("?","").replace(".","")
 
 
 
 
-
-#opens file entered by user
-
-
-file_1 = open(filename , "r" )
-
-#cleans file, eliminates unwanted punctuation 
-#eliminates text within brackets or parenthesis 
-#including the brackets or parathesis
-
-text = file_1.read().lower().replace("?","").replace(".","")
-text = re.sub("[\[].*?[\]]","", text)
-text = re.sub("[\(].*?[\)]","",text)
-
-if jeff != 1: 
-	text= text.replace("*","").replace(":","")
-
-
-
-file_1.close()
-
-
-# puts dyad words on file in a list 
+# puts dyad words on file in a list
 
 input = text.split()
-
+ 
 
 #replaces educated apostrophe with straight apostrophe
 
@@ -66,7 +41,7 @@ for i in input:
 	i_list.append(i.replace("\xe2\x80\x99","'"))
 
 input = i_list
- 
+
 
 
 
@@ -93,29 +68,29 @@ wanted = w_list
 
 
 
-#counts occurrences of the master list words in dyad words 
+#counts occurrences of the master list words in dyad words
 
 cnt = Counter()
 
 for item_wanted in wanted:
-	cnt [item_wanted] = 0	
-	for input_item in input: 
-		
-		if item_wanted == input_item: 
-			cnt[item_wanted] += 1 
+	cnt [item_wanted] = 0
+	for input_item in input:
+
+		if item_wanted == input_item:
+			cnt[item_wanted] += 1
 
 print cnt
 
 
 
-#write to CSV 
-   
-csver = filename.replace(".txt",".csv")
+#write to CSV
+
+csver = args.filename.replace(".txt",".csv")
 
 with open(csver,"wb") as csvfile:
-    fieldnames=["word","occurrences"] 
+    fieldnames=["word","occurrences"]
     writer=csv.writer(csvfile)
     writer.writerow(fieldnames)
-    
+
     for key, value in cnt.items():
-        writer.writerow([key] + [value]) 
+        writer.writerow([key] + [value])
