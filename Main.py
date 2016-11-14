26eb15ee320e95212f5bc6016be65978a3bdcea0
MarsRedSky <jmsiegel@ucsc.edu>
2016-11-14 09:10:24 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,98 +1,145 @@
-import re
-from collections import Counter
-import csv
-import argparse
+import re, csv, argparse
 
+def counts_words(dyad, master_l, jeff):
+    # recieves dyad from argparse
+    # opens dyad as file
+    # deletes comments in dyad
+    text = comment_deleter(open_file(dyad))
+    # opens master list
+    ml = open_file(master_l)
+    #checks for jeffersonian
+    if args.j:
+        # clean to jeff specifications
+        text = clean_jeff(text)
+        # mark overlaps and elongations
+        step = polish_jeff(text, ml)
+        #seperate input from wanted
+        input = step[0]
+        wanted = step[1]
+    else:
+        # clean to nonjeff specifications
+        input = clean(text)
+        wanted = ml
+    # counts words
+    # sends results to csv writer
+    csver(word_counter(input,wanted),dyad)
+
+
+
+    # csver(final_count(word_counter(input), clean_jeff(wanted)),dyad)
+
+def open_file(dyad):
+    #opens file, reads and lowers
+    with open(dyad, "r") as file:
+        file = file.read().lower()
+    return file
+
+def comment_deleter(file):
+    # delete comments surrounded by []
+    file = re.sub("[\[].*?[\]]", " ", file)
+    # delete comments surrounded by ()
+    file = re.sub("[\(].*?[\)]", " ", file)
+    return file
+
+def clean(text):
+    # deletes undesired characters
+    text_1 = text.replace("*", "").replace(":", "").replace("?", "").replace(".", "").replace("\xe2\x80\x99", "'")
+    return text_1
+
+def clean_jeff(text):
+    # deletes undesired characters
+    # leaves characters important to Jeffersonian conventions
+    text_1 = text.replace("?", "").replace(".", "").replace("\xe2\x80\x99", "'")
+    return text_1
+
+def polish_jeff(text,master_list):
+    # checks input words for overlap and elongation
+    # adds equivalent to wanted
+    ls = text.split()
+    ls_2 = master_list.split()
+    ls_1 = []
+    text_1 = ""
+    text_2 = ""
+    overlap = 0
+    # handles words with * and :
+    for word in ls:
+        if word.startswith("*") and word.endswith("*") ==0:
+            x = word[1:]
+            x_1 = x.replace(":", "")
+            word = "*"+x+"*"
+            ls_1.append(word)
+            overlap = 1
+            if x_1 in ls_2 and word not in ls_2:
+                ls_2.append(word)
+        elif word.endswith("*") and word.startswith("*") ==0:
+            x = word[:-1]
+            x_1 = x.replace(":","")
+            word = "*"+x+"*"
+            ls_1.append(word)
+            overlap = 0
+            if x_1 in ls_2 and word not in ls_2:
+                ls_2.append(word)
+        elif overlap == 1:
+            x = word
+            x_1 = x.replace(":","")
+            if x_1 in ls_2 and word not in ls_2:
+                ls_2.append(word)
+            word = "*"+x+"*"
+            ls_1.append(word)
+            if x in ls_2 and word not in ls_2:
+                ls_2.append(word)
+        elif word.startswith("*") and word.endswith("*"):
+            x = word[1:-1]
+            x_1 = x.replace(":","")
+            word = "*"+x+"*"
+            ls_1.append(word)
+            if x_1 in ls_2 and word not in ls_2:
+                ls_2.append(word)
+        else:
+            x = word
+            x_1 = x.replace(":","")
+            if x_1 in ls_2 and word not in ls_2:
+                ls_1.append(word)
+                ls_2.append(word)
+            else:
+                ls_1.append(word)
+    for word in ls_1:
+        text_1 += word + " "
+    for word_1 in ls_2:
+        text_2+= word_1 +" "
+
+    return text_1, text_2
+
+def word_counter(text,wanted):
+    # counts occurences of words in input
+    # increases count of wanted dict
+    dict = {}
+    input = text.split()
+    wanted = wanted.split()
+    for target in wanted:
+        dict[target] = 0
+    for word in input:
+        if word in dict:
+            dict[word]+=1
+    # "alphabetical" results
+    results = sorted(list(dict.items()))
+    return results
+
+def csver(data,filename):
+    # creates csv file named after dyad
+    csv_file = filename+".csv"
+    with open(csv_file, "w") as csv_file:
+        fieldnames = ["word", "occurrences"]
+        writer  = csv.writer(csv_file)
+        writer.writerow(fieldnames)
+        # writes rows of word and occurences
+        for row in data:
+            writer.writerow(row)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename') 
+parser.add_argument('filename')
 parser.add_argument('-j', action='store_true')
 args = parser.parse_args()
-
-file = open(args.filename, 'r') 
-file = file.read().lower()
-
-#deletes comments from dyads
-file = re.sub("[\[].*?[\]]"," ", file)
-file = re.sub("[\(].*?[\)]"," ", file)
-
-#Jeffersonian 
-if args.j:	
-	text = file.replace("?","").replace(".","")
-	print text
-
-#non-Jeffersonian
-else:
-	#deletes text surrounded by *s
-	file = re.sub("[\*].*?[\*]", "", file)
-	text = file.replace("*","").replace(":","").replace("?","").replace(".","")
-	print text
+counts_words(args.filename,"master_list",args.j)
 
 
-
-# puts dyad words on file in a list
-
-input = text.split()
- 
-
-#replaces educated apostrophe with straight apostrophe
-
-i_list = []
-
-for i in input:
-	i_list.append(i.replace("\xe2\x80\x99","'"))
-
-input = i_list
-
-
-
-
-#opens master list
-
-file = open ("master_list.txt" , "r")
-master = file.read().lower()
-file.close()
-
-
-
-#adds master list to a list
-
-wanted = master.split()
-
-# replaces educated apostrophe with straight apostrophe
-
-w_list = []
-
-for w in wanted:
-	w_list.append(w.replace("\xe2\x80\x99","'"))
-
-wanted = w_list
-
-
-
-#counts occurrences of the master list words in dyad words
-
-cnt = Counter()
-
-for item_wanted in wanted:
-	cnt [item_wanted] = 0
-	for input_item in input:
-
-		if item_wanted == input_item:
-			cnt[item_wanted] += 1
-
-print cnt
-
-
-
-#write to CSV
-
-csver = args.filename.replace(".txt",".csv")
-
-with open(csver,"wb") as csvfile:
-    fieldnames=["word","occurrences"]
-    writer=csv.writer(csvfile)
-    writer.writerow(fieldnames)
-
-    for key, value in cnt.items():
-        writer.writerow([key] + [value])
